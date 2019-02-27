dc269f16cee6a0c935eed1a07f383f55ac42c74d
Martin Liebeskind <martin@liebeskind.media>
2019-02-27 20:12:21 +0100

    multi backend usage

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Cell from './Cell';
+import MultiBackend from 'react-dnd-multi-backend';
 import TouchBackend from 'react-dnd-touch-backend';
+import HTML5Backend from 'react-dnd-html5-backend';
 import Preview from 'react-dnd-preview';
-import { DragDropContext } from 'react-dnd';
+import {DragDropContext} from 'react-dnd';
+
+const HTML5toTouch = {
+    backends: [
+        {
+            backend: HTML5Backend
+        },
+        {
+            backend: TouchBackend(), // Note that you can call your backends with options
+            preview: true,
+            transition: TouchTransition
+        }
+    ]
+};
 
 /**
  * Shuffles the passed array and returns a new one
@@ -12,121 +27,122 @@ import { DragDropContext } from 'react-dnd';
  * @return {Array}
  */
 function shuffle(a) {
-  const b = a.slice();
+    const b = a.slice();
 
-  for (let i = b.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [b[i], b[j]] = [b[j], b[i]];
-  }
+    for (let i = b.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [b[i], b[j]] = [b[j], b[i]];
+    }
 
-  return b;
+    return b;
 }
 
 class Puzzle extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    const { level } = props;
-    const cells = level * level;
+        const {level} = props;
+        const cells = level * level;
 
-    this.state = { positions: [...Array(cells).keys()] };
-  }
-
-  componentDidMount() {
-    const { positions } = this.state;
-
-    this.setState({ positions: shuffle(positions) });
-  }
-
-  onSwap(sourcePosition, dropPosition) {
-    const oldPositions = this.state.positions.slice();
-    const newPositions = [];
-    let done = true;
-    let p = 0;
-
-    for (let i in oldPositions) {
-      let value = oldPositions[i];
-      let newValue = value;
-
-      if (value === sourcePosition) {
-        newValue = dropPosition;
-      } else if (value === dropPosition) {
-        newValue = sourcePosition;
-      }
-
-      newPositions.push(newValue);
-
-      if (newValue !== p) {
-        done = false;
-      }
-
-      p = p + 1;
+        this.state = {positions: [...Array(cells).keys()]};
     }
 
-    this.setState({ positions: newPositions });
+    componentDidMount() {
+        const {positions} = this.state;
 
-    if(done) {
-      this.props.onDone();
+        this.setState({positions: shuffle(positions)});
     }
-  }
 
-  renderSquares() {
-    const { image, size, level } = this.props;
-    const { positions } = this.state;
+    onSwap(sourcePosition, dropPosition) {
+        const oldPositions = this.state.positions.slice();
+        const newPositions = [];
+        let done = true;
+        let p = 0;
 
-    const squares = positions.map((i) => {
-      return (
-        <Cell
-          key={i}
-          size={size}
-          image={image}
-          level={level}
-          position={i}
-          onSwap={this.onSwap.bind(this)}
-        />
-      );
-    })
+        for (let i in oldPositions) {
+            let value = oldPositions[i];
+            let newValue = value;
 
-    return squares;
-  }
+            if (value === sourcePosition) {
+                newValue = dropPosition;
+            } else if (value === dropPosition) {
+                newValue = sourcePosition;
+            }
 
-  previewGenerator(itemType, item, style) {
-    return <div class="item-list__item" style={style}>{itemType}</div>;
-  }
+            newPositions.push(newValue);
 
-  render() {
-    const { size } = this.props;
+            if (newValue !== p) {
+                done = false;
+            }
 
-    return (
-        <div>
-          <div
-              style={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                padding: 0,
-                width: `${size}px`,
-                height: `${size}px`
-              }}>
-            {this.renderSquares()}
-          </div>
-          <Preview generator={this.previewGenerator} />
-        </div>
+            p = p + 1;
+        }
 
-    );
-  }
+        this.setState({positions: newPositions});
+
+        if (done) {
+            this.props.onDone();
+        }
+    }
+
+    renderSquares() {
+        const {image, size, level} = this.props;
+        const {positions} = this.state;
+
+        const squares = positions.map((i) => {
+            return (
+                <Cell
+                    key={i}
+                    size={size}
+                    image={image}
+                    level={level}
+                    position={i}
+                    onSwap={this.onSwap.bind(this)}
+                />
+            );
+        })
+
+        return squares;
+    }
+
+    previewGenerator(itemType, item, style) {
+        return <div class="item-list__item" style={style}>{itemType}</div>;
+    }
+
+    render() {
+        const {size} = this.props;
+
+        return (
+            <div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        padding: 0,
+                        width: `${size}px`,
+                        height: `${size}px`
+                    }}>
+                    {this.renderSquares()}
+                </div>
+                <Preview generator={this.previewGenerator}/>
+            </div>
+
+        );
+    }
 };
 
 Puzzle.propTypes = {
-  image: PropTypes.string.isRequired,
-  size: PropTypes.number,
-  level: PropTypes.number,
-  onDone: PropTypes.func,
+    image: PropTypes.string.isRequired,
+    size: PropTypes.number,
+    level: PropTypes.number,
+    onDone: PropTypes.func,
 };
 
 Puzzle.defaultProps = {
-  size: 300,
-  level: 3,
-  onDone: () => {},
+    size: 300,
+    level: 3,
+    onDone: () => {
+    },
 };
 
-export default DragDropContext(TouchBackend)(Puzzle);
+export default DragDropContext(MultiBackend(HTML5toTouch))(Puzzle);
